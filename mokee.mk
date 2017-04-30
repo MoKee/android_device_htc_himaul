452a5cd0b43afb87a288a5e21357af80ca2acd47
martincz <martincz@mokeedev.com>
2017-04-30 23:30:35 +0800

    himaul: mokee-ify

--- mokee.mk
+++ mokee.mk
@@ -1,9 +1,9 @@
 $(call inherit-product, device/htc/himaul/full_himaul.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_himaul
+PRODUCT_NAME := mk_himaul
 BOARD_VENDOR := htc
 TARGET_VENDOR := htc
 PRODUCT_DEVICE := himaul
